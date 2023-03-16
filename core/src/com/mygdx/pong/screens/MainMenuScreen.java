71aa04a71dcb329d995ad69679dfce5271708ee6
Adam Bishop <adam.michael.bishop@gmail.com>
2023-03-16 13:50:57 -0500

    added main menu screen and logic to show the game screen

--- core/src/com/mygdx/pong/screens/MainMenuScreen.java
+++ core/src/com/mygdx/pong/screens/MainMenuScreen.java
@@ -1,19 +1,32 @@
 package com.mygdx.pong.screens;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.pong.Pong;
 
 public class MainMenuScreen implements Screen {
     private final Pong game;
-    private OrthographicCamera camera;
+    private final OrthographicCamera camera;
+    private final float viewportWidth;
+    private final float viewportHeight;
+    final String titleText = "PONG";
+    final String messageText = "press any key to start";
+    final GlyphLayout titleLayout;
+    final GlyphLayout messageLayout;
 
     public MainMenuScreen(final Pong game) {
         this.game = game;
+        this.viewportWidth = game.viewportWidth;
+        this.viewportHeight = game.viewportHeight;
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
+        camera.setToOrtho(false, game.viewportWidth, game.viewportHeight);
+        titleLayout = new GlyphLayout(game.fontLarge, titleText);
+        messageLayout = new GlyphLayout(game.fontMedium, messageText);
+        // or for non final texts: layout.setText(font, text);
     }
 
     @Override
@@ -23,15 +36,24 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0,0,0,1);
+        ScreenUtils.clear(0,0, 0,1);
 
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
+        float titleX = (viewportWidth - titleLayout.width) / 2;
+        float titleY = (viewportHeight + titleLayout.height) / 1.5f;
+        float messageX = (viewportWidth - messageLayout.width) / 2;
+        float messageY = titleY - 50;
 
         game.batch.begin();
-        game.font.draw(game.batch, "PONG", 100, 150);
-        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        game.fontLarge.draw(game.batch, titleLayout, titleX, titleY);
+        game.fontMedium.draw(game.batch, messageLayout, messageX, messageY);
         game.batch.end();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
     }
 
     @Override
