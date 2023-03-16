583dfb8ee0a5df3cce6b7cb9e8f6c7b9a200fc93
Adam Bishop <adam.michael.bishop@gmail.com>
2023-03-16 13:51:48 -0500

    added Bitmap font and FreeTypeFont

--- core/src/com/mygdx/pong/Pong.java
+++ core/src/com/mygdx/pong/Pong.java
@@ -1,32 +1,45 @@
 package com.mygdx.pong;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.mygdx.pong.screens.MainMenuScreen;
 
 public class Pong extends Game {
 	public SpriteBatch batch;
-	public BitmapFont font;
+	public BitmapFont fontSmall;
+	public BitmapFont fontMedium;
+	public BitmapFont fontLarge;
+	private FreeTypeFontGenerator generator;
+	public final float viewportWidth = 800;
+	public final float viewportHeight = 480;
 	
 	@Override
 	public void create () {
+		generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/PressStart2P-Regular.ttf"));
+		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		parameter.size = 12;
+		fontSmall = generator.generateFont(parameter);
+		parameter.size = 16;
+		fontMedium = generator.generateFont(parameter);
+		parameter.size = 24;
+		fontLarge = generator.generateFont(parameter);
+
 		batch = new SpriteBatch();
-		font = new BitmapFont();
 		this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
+		fontSmall.dispose();
+		generator.dispose();
 	}
 }
